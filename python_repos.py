86fa016bea5eded43b6b16f64b9e33560851e89c
bengkui2015@msn.cn <bengkui2015@msn.cn>
2019-03-29 09:36:36 +0800

    right

--- python_repos.py
+++ python_repos.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'https://api.github.com/search/repositories?q=launguge:python&sort=stars'
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
 print("Status code:", r.status_code)
 
